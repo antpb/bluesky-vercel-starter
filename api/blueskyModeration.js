a71ebc643ea420c470d47166955453d996823b53
antpb <itartist.pdf@gmail.com>
2023-05-14 12:43:31 -0500

    do batch calls intead of individual ones

--- api/blueskyModeration.js
+++ api/blueskyModeration.js
@@ -40,24 +40,25 @@ export default async function handler(req, res) {
     // Use GPT-3.5-turbo to moderate each batch
     const positiveIndexes = [];
     for (const batch of feedBatches) {
-      for (const item of batch) {
-        const response = await axios.post('https://api.openai.com/v1/chat/completions', {
-          model: "gpt-3.5-turbo",
-          messages: [{"role": "system", "content": "You are a helpful assistant that analyzes and categorizes sentiment. You will only respond with the word Positive or Negative for the provided text based on if it is closer to positive or negative."},
-                     {"role": "user", "content": `Analyze the sentiment of the following text: "${item.text}"`}],
-          temperature: 0.7
-        }, {
-          headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
-          }
-        });
+      const batchText = batch.map((item, i) => `Part ${i + 1}: ${item.text}`).join("\n\n");
+      const response = await axios.post('https://api.openai.com/v1/chat/completions', {
+        model: "gpt-3.5-turbo",
+		messages: [{"role": "system", "content": "You are a helpful assistant that analyzes and categorizes sentiment. You will only respond with the word Positive or Negative for the provided text based on if it is closer to positive or negative."},
+                   {"role": "user", "content": `Analyze the sentiment of each part of the following text separately: "${batchText}"`}],
+        temperature: 0.7
+      }, {
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
+        }
+      });
 
-        const aiMessage = response.data.choices[0].message.content;
+      const aiMessages = response.data.choices[0].message.content.split("\n\n");
 
-        // If the AI response is not negative, add the index to the positiveIndexes array
-        if (!aiMessage.includes("negative")) {
-          positiveIndexes.push(item.index);
+      // For each part, if the AI response is not negative, add the corresponding index to the positiveIndexes array
+      for (let i = 0; i < aiMessages.length; i++) {
+        if (!aiMessages[i].includes("negative")) {
+          positiveIndexes.push(batch[i].index);
         }
       }
     }
